9c769fc1ddd3a3b9fffbd98200113ab5df401e32
Nikita <NikitaPrusakov210@yandex.ru>
2024-08-10 17:37:07 +0300

    Disadvantages were eliminated

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,3 +1,4 @@
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
@@ -6,10 +7,12 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double EPSILON = 1E-6;
 
 string ReadLine() {
     string s;
@@ -83,12 +86,10 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
             [](const Document& lhs, const Document& rhs) {
-                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
                     return lhs.rating > rhs.rating;
                 }
-                else {
-                    return lhs.relevance > rhs.relevance;
-                }
+                return lhs.relevance > rhs.relevance;
             });
         if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
             matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
@@ -157,10 +158,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
@@ -246,7 +244,6 @@ void PrintDocument(const Document& document) {
         << "rating = "s << document.rating
         << " }"s << endl;
 }
-
 int main() {
     SearchServer search_server;
     search_server.SetStopWords("и в на"s);
